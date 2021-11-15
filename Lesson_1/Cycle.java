611e0ced9da030acf1f51fe3e5ab6bc219b529d8
sonic <vl040@mail.ru>
2021-11-15 16:40:53 +0300

    HW3

---
+++ Lesson_1/Cycle.java
@@ -0,0 +1,24 @@
+public class Cycle { 
+
+    public static void main(String[] agrs) {
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i);
+        }
+
+        int k = 6;
+        while(k != -8) {
+            System.out.println(k);
+            k -= 2;
+        }
+
+        int sum = 0;
+        int number = 10;
+        do {
+            if (number % 2 != 0) {
+                sum = number + sum;
+            }
+            number++;
+        } while(number != 20);
+        System.out.println(sum);
+    }
+}
