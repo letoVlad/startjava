82e6ca410f4787134d2eb8f5cb4a2f673a6e60ed
letoVlad <94187860+letoVlad@users.noreply.github.com>
2021-11-18 14:08:32 +0300

    Update MyFirstGame.java

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -6,15 +6,15 @@ public class MyFirstGame {
         int hiddenNumberComp = 27;
         int hiddenNumberUser = 0;
 
-        while (startRate <= endRate) {
+        while (true) {
             hiddenNumberUser = (startRate + endRate) / 2;
             if (hiddenNumberUser == hiddenNumberComp) {                
                 break;
             } else if (hiddenNumberUser < hiddenNumberComp) {
-                startRate = hiddenNumberUser + 1;
+                startRate = hiddenNumberUser;
                 System.out.println(hiddenNumberUser + " Данное число меньше того, что загадал компьютер");
             } else if (hiddenNumberUser > hiddenNumberComp) {
-                endRate = hiddenNumberUser - 1;
+                endRate = hiddenNumberUser;
                 System.out.println(hiddenNumberUser + " Данное число больше того, что загадал компьютер");
             }
         }
