628f0a547a127f4ff87b249e0b26f3db51d78080
sonic <vl040@mail.ru>
2021-11-18 20:19:09 +0300

    HW

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -3,21 +3,19 @@ public class MyFirstGame {
     public static void main(String[] agrs) {
         int startRate = 0;
         int endRate = 100;
-        int hiddenNumberComp = 27;
-        int hiddenNumber = 0;
+        int hiddenNumberComp = 46;
+        int playerNumber = 0;
 
-        while (hiddenNumber != hiddenNumberComp) {
-            hiddenNumber = (startRate + endRate) / 2;
-            if (hiddenNumber == hiddenNumberComp) {                
-                break;
-            } else if (hiddenNumber < hiddenNumberComp) {
-                startRate = hiddenNumber;
-                System.out.println(hiddenNumber + " Данное число меньше того, что загадал компьютер");
-            } else if (hiddenNumber > hiddenNumberComp) {
-                endRate = hiddenNumber;
-                System.out.println(hiddenNumber + " Данное число больше того, что загадал компьютер");
+        while (playerNumber != hiddenNumberComp) {
+            playerNumber = (startRate + endRate) / 2;
+            if (playerNumber < hiddenNumberComp) {
+                startRate = playerNumber + 1;
+                System.out.println(playerNumber + " Данное число меньше того, что загадал компьютер");
+            } else if (playerNumber > hiddenNumberComp) {
+                endRate = playerNumber - 1;
+                System.out.println(playerNumber + " Данное число больше того, что загадал компьютер");
             }
         }
-		System.out.println(hiddenNumber + " Поздравляю, число угадано!");
+        System.out.println(playerNumber + " Поздравляю, число угадано!");
     }
 }
