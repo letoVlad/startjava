68e821f59b5d6e78f8435056cf1b54a52a034f41
sonic <vl040@mail.ru>
2021-11-18 19:42:23 +0300

    HW

---
+++ Lesson_1/Unicode.java
@@ -0,0 +1,8 @@
+public class Unicode { 
+
+    public static void main(String[] agrs) {
+        for(char i = 33; i < 126; i++) {
+            System.out.println(i);
+        }
+    }
+}sssssssssssssssssssssssssssssssssssssssssss
