0c1f48167ee46d57c330d01a51667c5347e14cbe
sonic <vl040@mail.ru>
2021-11-21 19:54:57 +0300

    HW

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,28 +1,72 @@
-public class Wolf{
+public class Wolf {
 
-    String sex = "Male";
-    String alias = "Серый";
-    int weight = 33;
-    int age = 5;
-    String color = "Серый";
+    private String sex = "Male";
+    private String alias = "Серый";
+    private int weight = 33;
+    private int age = 5;
+    private String color = "Серый";
 
-    void go() {
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+     public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+     public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void go() {
         System.out.println(alias + " идет бредет");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println(alias + " присел на дорожку");
     }
 
-    void run() {
-        System.out.println(alias + " бежит за добычей");
+    public String run() {
+        return "бежит за добычей";
     }
 
-    void howl() {
+    public void howl() {
         System.out.println(alias + " воет на луну");
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println(alias + " выслеживает добычу");
     }
 }
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,16 +2,26 @@ public class WolfTest  {
     
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        wolfOne.sex = "Female";
-        wolfOne.age = 1;
-        wolfOne.weight = 27;
-        System.out.println("Кличка волчка " + wolfOne.alias);
-        System.out.println("Волчку теперь " + wolfOne.age + " лет");
-        System.out.println("Вес волка = " + wolfOne.weight);
+        // wolfOne.sex = "Female";
+        // wolfOne.age = 1;
+        // wolfOne.weight = 27;
+        // System.out.println("Кличка волчка " + wolfOne.alias);
+        // System.out.println("Волчку теперь " + wolfOne.age + " лет");
+        // System.out.println("Вес волка = " + wolfOne.weight);
         wolfOne.run();
         wolfOne.sit();
         wolfOne.hunt();
         wolfOne.howl();
         wolfOne.go();
+        // Новые значения
+        wolfOne.setAlias("Black");
+        wolfOne.setAge(5);
+        wolfOne.setColor("White");
+        wolfOne.setWeight(30);
+        System.out.println(wolfOne.getAlias());
+        System.out.println(wolfOne.getAge());
+        wolfOne.setAge(123);
+        System.out.println(wolfOne.getColor());
+        System.out.println(wolfOne.getWeight());
     }
 }
